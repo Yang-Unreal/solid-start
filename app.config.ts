19e8f9885ced65d66881190fc4d6b873dd6534bf
unknown <y953159141@gmail.com>
2025-06-08 23:28:08 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: false, // Ensure all CSS is bundled into a single file
+    },
   },
 });
