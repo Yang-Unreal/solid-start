8878e0469b4946b4783d3f05aa2c768fbe458286
Yang-Unreal <65805377+Yang-Unreal@users.noreply.github.com>
2025-07-31 13:35:39 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,14 +7,16 @@ export default function Home() {
     <main>
       <div class="relative min-h-screen flex items-center justify-center overflow-hidden bg-cover bg-center bg-[url('/heroBackground.webp')]">
         <div class="absolute inset-0 bg-black opacity-20"></div>
-        <h1 class="absolute text-light-gray text-[150px] font-formula-bold top-2/5 text-center leading-35">
+        <h1 class="absolute text-light-gray text-7xl md:text-[150px] font-formula-bold top-2/5 text-center leading-18 md:leading-35">
           CHINA SOURCING <br />
           SIMPLIFIED
         </h1>
         <div class="absolute flex h-auto w-full bottom-20 container-padding gap-10 ">
           <div class="w-1/3 items-center justify-center text-center text-white"></div>
           <div class="w-1/3 text-white text-center">
-            <h1 class="text-3xl font-formula-bold">PRE-VETTED PRODUCTS</h1>
+            <h1 class="text-lg md:text-3xl font-formula-bold">
+              PRE-VETTED PRODUCTS
+            </h1>
             <button class="border rounded-full px-5 py-2 text-sm mt-2">
               Browse Our Store
             </button>
