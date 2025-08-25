42cf7a02fa8e7198430f51e20389b20bd351f5a4
Yang-Unreal <65805377+Yang-Unreal@users.noreply.github.com>
2025-08-25 09:43:14 +0800

    scroll fix test

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,10 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
+if (history.scrollRestoration) {
+  history.scrollRestoration = "manual";
+}
+
 mount(() => {
   return <StartClient />;
 }, document.getElementById("app")!);
