bcf311a119edfc2643eb86bfbb3400a22a344c96
Yang-Unreal <65805377+Yang-Unreal@users.noreply.github.com>
2025-10-31 14:07:47 +0800

    preloader test 2

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -4,8 +4,6 @@ import { useLenis } from "~/context/LenisContext";
 import YourLogo from "./logo/YourLogo";
 import MobileLogo from "./logo/MobileLogo";
 export default function Preloader() {
-  const [showPreloader, setShowPreloader] = createSignal(true);
-
   let preloaderRef: HTMLDivElement | undefined;
   let logoContainerRef: HTMLDivElement | undefined;
   let copyrightRef: HTMLDivElement | undefined;
@@ -20,7 +18,6 @@ export default function Preloader() {
     const tl = gsap.timeline({
       onComplete: () => {
         lenis?.start();
-        setShowPreloader(false);
       },
     });
 
@@ -76,11 +73,13 @@ export default function Preloader() {
     const columns = preloaderRef?.querySelectorAll(".column");
     if (columns) {
       gsap.set(columns, {
+        scale: 1.1,
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
       });
       tl.to(
         columns,
         {
+          scale: 1,
           y: "-100vh",
           duration: 0.6,
           ease: "circ.inOut",
@@ -91,7 +90,7 @@ export default function Preloader() {
       tl.to(
         columns,
         {
-          clipPath: "polygon(0% 0%, 100% 0%, 100% 92%, 0% 100%)",
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 94%, 0% 100%)",
           duration: 0.6,
           ease: "circ.inOut",
           stagger: 0.03,
@@ -104,11 +103,13 @@ export default function Preloader() {
     const columns2 = preloaderRef?.querySelectorAll(".column2");
     if (columns2) {
       gsap.set(columns2, {
+        scale: 1.1,
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
       });
       tl.to(
         columns2,
         {
+          scale: 1,
           y: "-100vh",
           duration: 0.6,
           ease: "circ.inOut",
@@ -119,7 +120,7 @@ export default function Preloader() {
       tl.to(
         columns2,
         {
-          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 92%)",
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 94%)",
           duration: 0.6,
           ease: "circ.inOut",
           stagger: 0.03,
@@ -130,55 +131,50 @@ export default function Preloader() {
   });
 
   return (
-    <Show when={showPreloader()}>
-      <div
-        ref={preloaderRef}
-        class="loading-container justify-center items-center"
-      >
-        {/* Background columns */}
-        <div class="loading-container absolute inset-0">
-          <div class="column flex h-full w-full bg-dark rounded"></div>
-          <div class="column flex h-full w-full bg-dark rounded"></div>
-          <div class="column flex h-full w-full bg-dark rounded"></div>
-          <div class="column flex h-full w-full bg-dark rounded"></div>
-          <div ref={logoContainerRef} class="logo">
-            <YourLogo class="h-auto w-full text-gray" />
-            <YourLogo class="h-auto w-full text-light absolute invisible" />
-          </div>
-          <div ref={copyrightRef} class="copyright-row">
-            <div class="copyright-visual">
-              <div
-                class="aspect-square h-full border border-gray/25 flex justify-center items-center"
-                style="border-radius: 0 var(--border-radius) var(--border-radius) 0;"
-              >
-                <MobileLogo class="text-gray/25 w-10 h-auto" />
+    <div ref={preloaderRef}>
+      {/* Background columns */}
+      <div class="loading-container absolute inset-0">
+        <div class="column flex h-full w-full bg-dark rounded"></div>
+        <div class="column flex h-full w-full bg-dark rounded"></div>
+        <div class="column flex h-full w-full bg-dark rounded"></div>
+        <div class="column flex h-full w-full bg-dark rounded"></div>
+        <div ref={logoContainerRef} class="logo">
+          <YourLogo class="h-auto w-full text-gray" />
+          <YourLogo class="h-auto w-full text-light absolute invisible" />
+        </div>
+        <div ref={copyrightRef} class="copyright-row">
+          <div class="copyright-visual">
+            <div
+              class="aspect-square h-full border border-gray/25 flex justify-center items-center"
+              style="border-radius: 0 var(--border-radius) var(--border-radius) 0;"
+            >
+              <MobileLogo class="text-gray/25 w-10 h-auto" />
+            </div>
+            <div
+              class="flex flex-col border border-gray/25 border-l-0 text-gray/25"
+              style="border-radius: 0 var(--border-radius) var(--border-radius) 0;"
+            >
+              <div class="flex border-b border-gray/25  justify-center items-center py-[0.3em] px-[0.35em] font-formula-bold uppercase">
+                <h4 class="text-[1rem] leading-[1.1] tracking-[0.02em]">
+                  2025 © All rights reserved
+                </h4>
               </div>
-              <div
-                class="flex flex-col border border-gray/25 border-l-0 text-gray/25"
-                style="border-radius: 0 var(--border-radius) var(--border-radius) 0;"
-              >
-                <div class="flex border-b border-gray/25  justify-center items-center py-[0.3em] px-[0.35em] font-formula-bold uppercase">
-                  <h4 class="text-[1rem] leading-[1.1] tracking-[0.02em]">
-                    2025 © All rights reserved
-                  </h4>
-                </div>
-                <div class="flex  justify-center items-center py-[0.4em] px-[0.3em] text-center overflow-hidden min-h-[1.72em]">
-                  <p class="text-[0.425em]">
-                    LIMING is a Export Company specializing in Used Car Parallel
-                    Exports from China.
-                  </p>
-                </div>
+              <div class="flex  justify-center items-center py-[0.4em] px-[0.3em] text-center overflow-hidden min-h-[1.72em]">
+                <p class="text-[0.425em]">
+                  LIMING is a Export Company specializing in Used Car Parallel
+                  Exports from China.
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div class="transition-container">
-          <div class="column2 flex h-full w-full bg-darkgray rounded"></div>
-          <div class="column2 flex h-full w-full bg-darkgray rounded"></div>
-          <div class="column2 flex h-full w-full bg-darkgray rounded"></div>
-          <div class="column2 flex h-full w-full bg-darkgray rounded"></div>
-        </div>
       </div>
-    </Show>
+      <div class="transition-container">
+        <div class="column2 flex h-full w-full bg-darkgray rounded"></div>
+        <div class="column2 flex h-full w-full bg-darkgray rounded"></div>
+        <div class="column2 flex h-full w-full bg-darkgray rounded"></div>
+        <div class="column2 flex h-full w-full bg-darkgray rounded"></div>
+      </div>
+    </div>
   );
 }
