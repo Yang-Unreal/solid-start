111a4716323abd230599ab2c47b0a19a1c7a50ae
Yang-Unreal <65805377+Yang-Unreal@users.noreply.github.com>
2025-11-24 09:37:21 +0800

    vh test

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -1,7 +1,7 @@
 import gsap from "gsap/all";
 import { CustomEase } from "gsap/CustomEase";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { type JSX, onMount, Suspense } from "solid-js";
+import { type JSX, onCleanup, onMount, Suspense } from "solid-js";
 import Header from "./Header";
 import MenuDrawer from "./nav/MenuDrawer";
 import Preloader from "./Preloader";
@@ -14,6 +14,19 @@ export function AppContent(props: { children: JSX.Element }) {
 		// Create custom easings
 		CustomEase.create("custom", "M0,0 C0.25,0.1 0.25,1 1,1");
 		CustomEase.create("slideUp", "M0,0 C0.343,0.923 0.137,1.011 1,1 ");
+
+		// Viewport Height Fix
+		const setVh = () => {
+			const vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty("--vh", `${vh}px`);
+		};
+
+		setVh();
+		window.addEventListener("resize", setVh);
+
+		onCleanup(() => {
+			window.removeEventListener("resize", setVh);
+		});
 	});
 
 	return (
